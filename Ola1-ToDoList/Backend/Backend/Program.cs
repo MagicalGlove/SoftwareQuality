27de6115b7c23c276be21b0b9644bfaacc31edba
Mads Knudsen <35879356+Mfknudsen@users.noreply.github.com>
2024-09-04 14:31:14 +0200

    Working mongodb connection

--- Ola1-ToDoList/Backend/Backend/Program.cs
+++ Ola1-ToDoList/Backend/Backend/Program.cs
@@ -1,9 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 
-Main();
-return ;
-
-static void Main()
+namespace Backend
 {
-    
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Starting");
+            const string connectionString =
+                "mongodb+srv://test:1234@ola2cluster.bijj3hs.mongodb.net/?retryWrites=true&w=majority&appName=Ola2Cluster";
+            MongoClient client = new MongoClient(connectionString);
+
+            IMongoCollection<BsonDocument>? collection =
+                client.GetDatabase("sqOla1").GetCollection<BsonDocument>("Tasks");
+
+            FilterDefinition<BsonDocument>? filter = Builders<BsonDocument>.Filter.Eq("name", "task 1");
+
+            BsonDocument? document = collection.Find(filter).First();
+
+            Console.WriteLine(document.ToString());
+        }
+    }
 }
