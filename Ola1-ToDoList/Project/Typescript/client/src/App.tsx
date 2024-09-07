074352c6c9d851bf96580dd33a9f64a9cde5adb3
MagicalGlove <sundlivnu@gmail.com>
2024-09-07 18:20:46 +0200

    Adds complete status display

--- Ola1-ToDoList/Project/Typescript/client/src/App.tsx
+++ Ola1-ToDoList/Project/Typescript/client/src/App.tsx
@@ -5,41 +5,47 @@ import { deleteTask } from "./utils/deleteTask";
 import TaskItem from "./components/TaskItem";
 
 const App = () => {
-  const [tasks, setTasks] = useState<Task[]>()
+  const [tasks, setTasks] = useState<Task[]>();
+
   const fetchTasks = async () => {
     try {
       const tasks = await getAllTasksAPI();
-      console.log('Tasks:', tasks);
-      setTasks(tasks)
+      console.log("Tasks:", tasks);
+      setTasks(tasks);
     } catch (error) {
-      console.error('Error fetching tasks:', error);
+      console.error("Error fetching tasks:", error);
     }
   };
 
   useEffect(() => {
-    fetchTasks()
+    fetchTasks();
   }, []);
 
   async function handleButtonClick(id: number): Promise<void> {
     try {
-      await deleteTask(id);  
-      console.log('Task deleted:', id);
-
-     
-      await fetchTasks();  
+      await deleteTask(id);
+      console.log("Task deleted:", id);
+      await fetchTasks();
     } catch (error) {
-      console.error('Error deleting task:', error);
+      console.error("Error deleting task:", error);
     }
   }
 
   return (
     <div className="App">
-  {tasks?.map((task, index) => (
-    <TaskItem task={task} onDelete={handleButtonClick}/>
-  ))}
-</div>
-
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          padding: "20px",
+        }}
+      >
+        {tasks?.map((task, index) => (
+          <TaskItem key={index} task={task} onDelete={handleButtonClick} />
+        ))}
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
--- Ola1-ToDoList/Project/Typescript/client/src/components/TaskItem.tsx
+++ Ola1-ToDoList/Project/Typescript/client/src/components/TaskItem.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
 import { Task } from "../types/tasks";
-
+import checkmark from "../misc/Checkmark.png";
+import XMark from "../misc/X-Mark.png";
 
 interface TaskItemProps {
   task: Task;
@@ -8,11 +9,43 @@ interface TaskItemProps {
 }
 
 const TaskItem: React.FC<TaskItemProps> = ({ task, onDelete }) => {
+  console.log(task);
+
   return (
-    <div style={{ border: "1px solid black", backgroundColor: "powderblue", padding: "10px", margin: "10px", borderRadius: "5px" }}>
+    <div
+      style={{
+        border: "1px solid black",
+        backgroundColor: "#ced1de",
+        padding: "10px",
+        margin: "10px",
+        borderRadius: "5px",
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "200px",
+      }}
+    >
       <h2>{task.text}</h2>
       <p>{task.id}</p>
-      <button style={{ backgroundColor: 'red' }} onClick={() => onDelete(task.id)}>Delete</button>
+      <button
+        style={{ backgroundColor: "red", marginBottom: "10px" }}
+        onClick={() => onDelete(task.id)}
+      >
+        Delete
+      </button>
+
+      <img
+        src={task.isCompleted ? checkmark : XMark}
+        alt="logo"
+        style={{
+          position: "absolute",
+          top: "10px",
+          right: "10px",
+          width: "30px",
+          height: "30px",
+        }}
+      />
     </div>
   );
 };
