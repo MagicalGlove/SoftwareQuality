da8a1553b7dd8972bd63dbc38494c4e16007cdc9
MagicalGlove <sundlivnu@gmail.com>
2024-09-08 13:25:30 +0200

    Edit task complete and styling

--- Ola1-ToDoList/Project/Typescript/client/src/App.tsx
+++ Ola1-ToDoList/Project/Typescript/client/src/App.tsx
@@ -7,12 +7,15 @@ import { addTask } from "./utils/addTask";
 import "./App.css";
 import checkmark from "./misc/Checkmark.png";
 import xMark from "./misc/X-Mark.png";
+import edit from "./misc/Edit.png";
 
 const App = () => {
   const [tasks, setTasks] = useState<Task[]>();
+  const [editTask, setEditTask] = useState<Task>();
   const [text, setText] = useState<string>("");
   const [deadline, setDeadline] = useState<string>("");
   const [selectedCategory, setSelectedCategory] = useState<number>(0);
+  const [editTaskId, setEditTaskId] = useState<string>("");
 
   const fetchTasks = async () => {
     try {
@@ -40,16 +43,37 @@ const App = () => {
     fetchTasks();
   }, []);
 
-  const handleEditTask = async (e: React.ChangeEvent<HTMLSelectElement>, task: Task) => {
+  const handleEditFullTask = async () => {
+    console.log("Check");
+
+    try {
+      const editedTask = {
+        ...editTask,
+        text: text,
+        deadline: deadline,
+      };
+      console.log(editedTask);
+      const updatedTask = await editTaskAPI(editedTask);
+      console.log("Updated task:", updatedTask);
+      fetchTasks();
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+  const handleEditTask = async (
+    e: React.ChangeEvent<HTMLSelectElement>,
+    task: Task
+  ) => {
     try {
       const editedTask = {
         ...task,
-        category: parseInt(e.target.value)
+        category: parseInt(e.target.value),
       };
-      console.log(editedTask)
+      console.log(editedTask);
       const updatedTask = await editTaskAPI(editedTask);
       console.log("Updated task:", updatedTask);
-      fetchTasks()
+      fetchTasks();
     } catch (error) {
       console.error("Error fetching tasks:", error);
     }
@@ -83,14 +107,44 @@ const App = () => {
     setSelectedCategory(category);
   };
 
-  const filteredTasks = selectedCategory === 0 
-    ? tasks 
-    : tasks?.filter(task => task.category === selectedCategory);
+  async function handleEdit(
+    id: string | undefined,
+    isCompleted: boolean | undefined
+  ): Promise<void> {
+    if (!id || typeof isCompleted !== "boolean") return;
+    try {
+      await completedTask(id, isCompleted);
+      console.log("Task completed/uncompleted:", id);
+      await fetchTasks();
+    } catch (error) {
+      console.error("Error deleting task:", error);
+    }
+  }
+
+  const filteredTasks =
+    selectedCategory === 0
+      ? tasks
+      : tasks?.filter((task) => task.category === selectedCategory);
 
   return (
     <div className="App">
-      <div className="container">
-        <div className="header">
+      <div className="container" style={{ backgroundColor: "aliceblue" }}>
+        <div className="header" style={{ marginBottom: "20px" }}>
+          <input
+            style={{
+              backgroundColor: "white",
+              borderColor: "black",
+              borderWidth: "1.8px",
+            }}
+            type="text"
+            disabled
+            value={
+              editTask?.id
+                ? `Edit task: ${editTask?.id}`
+                : 'Click "edit" on a task'
+            }
+            className="input-field"
+          />
           <input
             type="text"
             placeholder="Task Text"
@@ -105,15 +159,58 @@ const App = () => {
             onChange={(e) => setDeadline(e.target.value)}
             className="input-field"
           />
-          <button className="add-task-button" onClick={handleAddTask}>
+          <button
+            style={{
+              marginRight: "20px",
+              backgroundColor: "lightgreen",
+              borderRadius: "8px",
+            }}
+            className="add-task-button"
+            onClick={handleEditFullTask}
+          >
+            Edit Task
+          </button>
+          <button
+            style={{ backgroundColor: "lightgreen", borderRadius: "8px" }}
+            className="add-task-button"
+            onClick={handleAddTask}
+          >
             Add Task
           </button>
         </div>
         <div className="tabs">
-          <button onClick={() => handleTabChange(0)}>All</button>
-          <button onClick={() => handleTabChange(1)}>Work</button>
-          <button onClick={() => handleTabChange(2)}>Chores</button>
-          <button onClick={() => handleTabChange(3)}>Leisure</button>
+          <button
+            style={{
+              backgroundColor: selectedCategory == 0 ? "lightgreen" : "white",
+            }}
+            onClick={() => handleTabChange(0)}
+          >
+            All
+          </button>
+          <button
+            style={{
+              backgroundColor: selectedCategory == 1 ? "lightgreen" : "white",
+            }}
+            onClick={() => handleTabChange(1)}
+          >
+            Work
+          </button>
+          <button
+            style={{
+              backgroundColor: selectedCategory == 2 ? "lightgreen" : "white",
+            }}
+            onClick={() => handleTabChange(2)}
+          >
+            Chores
+          </button>
+          <button
+            style={{
+              backgroundColor: selectedCategory == 3 ? "lightgreen" : "white",
+            }}
+            onClick={() => handleTabChange(3)}
+          >
+            Leisure
+          </button>
         </div>
         <div className="table-container">
           <table className="table-content">
@@ -124,6 +221,7 @@ const App = () => {
                 <th className="table-cell table-cell-header">Set category</th>
                 <th className="table-cell table-cell-header">Completed</th>
                 <th className="table-cell table-cell-header">Delete</th>
+                <th className="table-cell table-cell-header">Edit</th>
               </tr>
             </thead>
             <tbody>
@@ -132,16 +230,20 @@ const App = () => {
                   <td className="table-cell">{task.text}</td>
                   <td className="table-cell">{task.deadline || "-"}</td>
                   <td className="table-cell">
-                    <select value={task.category} onChange={(e) => handleEditTask(e, task)}>
+                    <select
+                      value={task.category}
+                      onChange={(e) => handleEditTask(e, task)}
+                    >
                       <option value="0">None</option>
                       <option value="1">Work</option>
                       <option value="2">Chores</option>
                       <option value="3">Leisure</option>
-                    </select></td>
+                    </select>
+                  </td>
                   <td className="table-cell">
                     <img
                       src={task.isCompleted ? checkmark : xMark}
-                      alt="logo"
+                      alt={task.isCompleted ? "complete" : "incomplete"}
                       style={{
                         width: "30px",
                         height: "30px",
@@ -158,11 +260,36 @@ const App = () => {
                         backgroundColor: "red",
                         marginBottom: "10px",
                         cursor: "pointer",
+                        border: "2px solid black",
+                        borderRadius: "4px",
                       }}
                       onClick={() => handleButtonClick(task.id)}
                     >
                       Delete
                     </button>
+                  </td>
+                  <td className="table-cell">
+                    <img
+                      src={edit}
+                      alt="Edit"
+                      style={{
+                        width: "25px",
+                        height: "25px",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => {
+                        setEditTask(task);
+                        setText(task?.text || "N/A");
+                        setDeadline(
+                          task?.deadline ||
+                            (() => {
+                              const today = new Date();
+                              today.setDate(today.getDate() + 7);
+                              return today.toISOString().split("T")[0];
+                            })()
+                        );
+                      }}
+                    />
                   </td>
                 </tr>
               ))}
