2dee5a95028be012bd06f1099c43f6310bc036dd
SMerved <90383268+SMerved@users.noreply.github.com>
2024-09-18 04:01:37 +0200

    fd

---
+++ Ola2/Project/client/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  transformIgnorePatterns: ['/node_modules/'],
+};
+
+export default config;
