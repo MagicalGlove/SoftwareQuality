629db41dffbc007eb32f4c8fe497659179e9b7c4
Mads Knudsen <35879356+Mfknudsen@users.noreply.github.com>
2024-09-19 12:37:08 +0200

    Boundary test 1

--- ola3/Project/api/src/db_functions/logicChecks.ts
+++ ola3/Project/api/src/db_functions/logicChecks.ts
@@ -2,7 +2,7 @@ import {Task} from '../entities/Task';
 
 function checkAddTaskBoundary(text: string, deadline: string | undefined | null, isCompleted: boolean | undefined) {
     if (text.length < 3 || text.length > 100) {
-        throw new Error("Text must be between 3 and 100 characters");
+        throw new Error("Text must be between 3 and 100 characters " + text.length.toString());
     }
     if (deadline === null) {
         throw new Error("Deadline is required");
