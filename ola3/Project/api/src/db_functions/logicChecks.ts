b9a4954dc2a356f92aec7ed5c49c956ac977e0ec
Mads Knudsen <madsfknudsen@hotmail.com>
2024-09-27 12:30:01 +0200

    business rules and tests done

--- ola3/Project/api/src/db_functions/logicChecks.ts
+++ ola3/Project/api/src/db_functions/logicChecks.ts
@@ -1,15 +1,28 @@
 function checkAddTaskBoundary(text: string, description: string, deadline: string | undefined | null, isCompleted: boolean | undefined) {
     if (text.length < 3 || text.length > 100) {
-        throw new Error("Text must be between 3 and 100 characters " + text.length.toString());
+        throw new Error("Text must be between 3 and 100 characters ");
     }
-    if (description === null) {
+    if (description.length < 5) {
+        throw new Error("Description must be at least 5 characters");
+    }
+    if (deadline === null || deadline == undefined) {
         throw new Error("Deadline is required");
     }
-    if (deadline === null) {
-        throw new Error("Deadline is required");
+
+    let deadlineTime = new Date(deadline);
+    let currentTime = new Date();
+    let diff = deadlineTime.valueOf() - currentTime.valueOf();
+    diff /= 86400000 ;
+    diff = Math.round(diff)
+    if (diff < 0) {
+        throw new Error("Deadline must be greater than or equal to current date | " + diff)
     }
-    if  (isCompleted === null) {
-        throw new Error("Text is required");
+    if (diff > 100) {
+        throw new Error("Deadline must be within a realistic time limit | " + diff)
+    }
+
+    if (isCompleted === undefined) {
+        throw new Error("Is Complete is required");
     }
 }
 
--- ola3/Project/api/src/tests/boundaryEquivalence.test.ts
+++ ola3/Project/api/src/tests/boundaryEquivalence.test.ts
@@ -13,7 +13,7 @@ describe("title size equivalence", () => {
             }
 
             expect(() => {
-                checkAddTaskBoundary(testTitle, 'description', "undefined", false);
+                checkAddTaskBoundary(testTitle, 'description', new Date().toString(), false);
             }).toThrow(Error);
         }
     });
@@ -27,7 +27,7 @@ describe("title size equivalence", () => {
                 testTitle += "x";
             }
             expect(() => {
-                checkAddTaskBoundary(testTitle, 'description', "undefined", false);
+                checkAddTaskBoundary(testTitle, 'description', new Date().toString(), false);
             }).not.toThrow(Error);
         }
     });
@@ -42,7 +42,7 @@ describe("title size equivalence", () => {
             }
 
             expect(() => {
-                checkAddTaskBoundary(testTitle, 'description', "undefined", false);
+                checkAddTaskBoundary(testTitle, 'description', new Date().toString(), false);
             }).toThrow(Error);
         }
     });
@@ -59,7 +59,7 @@ describe("title size boundary", () => {
             }
 
             expect(() => {
-                checkAddTaskBoundary(testTitle, 'description', "undefined", false);
+                checkAddTaskBoundary(testTitle, 'description', new Date().toString(), false);
             }).toThrow(Error);
         }
     });
@@ -72,7 +72,7 @@ describe("title size boundary", () => {
             }
 
             expect(() => {
-                checkAddTaskBoundary(testTitle, 'description', "undefined", false);
+                checkAddTaskBoundary(testTitle, 'description', new Date().toString(), false);
             }).not.toThrow(Error);
         }
     });
@@ -85,11 +85,191 @@ describe("title size boundary", () => {
             }
 
             expect(() => {
-                checkAddTaskBoundary(testTitle, 'description', "undefined", false);
+                checkAddTaskBoundary(testTitle, 'description', new Date().toString(), false);
             }).toThrow(Error);
         }
     });
 });
+
+describe("dateline size equivalence", () => {
+    const partition1 = [-10, -1], partition2 = [0, 100], partition3 = [101, 1000];
+
+    let originalDate = new Date();
+
+    test("Partition 1 Invalid", () => {
+        for (let i = 0; i < 3; i++) {
+            let l: number = getRandomInt(partition1[0], partition1[1]);
+
+            let nextDate: Date = new Date();
+            nextDate.setDate(originalDate.getDate() + l)
+
+            expect(() => {
+                checkAddTaskBoundary("Test", "description", nextDate.toString(), false)
+            }).toThrow(Error)
+        }
+    });
+
+    test("Partition 2 Valid", () => {
+        for (let i = 0; i < 3; i++) {
+            let l: number = getRandomInt(partition2[0], partition2[1]);
+
+            let nextDate: Date = new Date();
+            nextDate.setDate(originalDate.getDate() + l)
+
+            expect(() => {
+                checkAddTaskBoundary("Test", "description", nextDate.toString(), false)
+            }).not.toThrow(Error)
+        }
+    });
+
+    test("Partition 3 Invalid", () => {
+        for (let i = 0; i < 3; i++) {
+            let l: number = getRandomInt(partition3[0], partition3[1]);
+
+            let nextDate: Date = new Date();
+            nextDate.setDate(originalDate.getDate() + l)
+
+            expect(() => {
+                checkAddTaskBoundary("Test", "description", nextDate.toString(), false)
+            }).toThrow(Error)
+        }
+    });
+})
+
+describe("dateline size boundary", () => {
+    const partition1 = [-10, -1], partition2 = [0, 100], partition3 = [101, 1000];
+    let originalDate = new Date();
+
+    test("Partition 1 Invalid", () => {
+        for (let i = 0; i < 2; i++) {
+            let l: number = partition1[i];
+
+            let nextDate: Date = new Date();
+            nextDate.setDate(originalDate.getDate() + l)
+
+            expect(() => {
+                checkAddTaskBoundary("title", "testDescription", nextDate.toString(), false);
+            }).toThrow(Error);
+        }
+    });
+
+    test("Partition 2 Valid", () => {
+        for (let i = 0; i < 2; i++) {
+            let l: number = partition2[i];
+
+            let nextDate: Date = new Date();
+            nextDate.setDate(originalDate.getDate() + l)
+
+            expect(() => {
+                checkAddTaskBoundary("title", "testDescription", nextDate.toString(), false);
+            }).not.toThrow(Error);
+        }
+    });
+
+    test("Partition 3 Invalid", () => {
+        for (let i = 0; i < 2; i++) {
+            let l: number = partition3[i];
+
+            let nextDate: Date = new Date();
+            nextDate.setDate(originalDate.getDate() + l)
+
+            expect(() => {
+                checkAddTaskBoundary("title", "testDescription", nextDate.toString(), false);
+            }).toThrow(Error);
+        }
+    });
+})
+
+describe("description size equivalence", () => {
+    const partition1 = [0, 4], partition2 = [5, 100], partition3 = [101, 1000];
+
+    test("Partition 1 Invalid", () => {
+        for (let i = 0; i < 3; i++) {
+            const l = getRandomInt(partition1[0], partition1[1]);
+
+            let testDescription: string = "";
+            for (let j = 0; j < l; j++) {
+                testDescription += "x";
+            }
+
+            expect(() => {
+                checkAddTaskBoundary("title", testDescription, new Date().toString(), false);
+            }).toThrow(Error);
+        }
+    });
+
+    test("Partition 2 Valid", () => {
+        for (let i = 0; i < 3; i++) {
+            const l = getRandomInt(partition2[0], partition2[1]);
+
+            let testDescription: string = "";
+            for (let j = 0; j < l; j++) {
+                testDescription += "x";
+            }
+            expect(() => {
+                checkAddTaskBoundary("title", testDescription, new Date().toString(), false);
+            }).not.toThrow(Error);
+        }
+    });
+
+    test("Partition 3 Valid", () => {
+        for (let i = 0; i < 3; i++) {
+            const l = getRandomInt(partition3[0], partition3[1]);
+
+            let testDescription: string = "";
+            for (let j = 0; j < l; j++) {
+                testDescription += "x";
+            }
+
+            expect(() => {
+                checkAddTaskBoundary("title", testDescription, new Date().toString(), false);
+            }).not.toThrow(Error);
+        }
+    });
+})
+
+describe("description size boundary", () => {
+    const partition1 = [0, 4], partition2 = [5, 100], partition3 = [101, 1000];
+
+    test("Partition 1 Invalid", () => {
+        for (let i = 0; i < 2; i++) {
+            let testDescription: string = "";
+            for (let j = 0; j < partition1[i]; j++) {
+                testDescription += "x";
+            }
+
+            expect(() => {
+                checkAddTaskBoundary("title", testDescription, new Date().toString(), false);
+            }).toThrow(Error);
+        }
+    });
+
+    test("Partition 2 Valid", () => {
+        for (let i = 0; i < 2; i++) {
+            let testDescription: string = "";
+            for (let j = 0; j < partition2[i]; j++) {
+                testDescription += "x";
+            }
+
+            expect(() => {
+                checkAddTaskBoundary("title", testDescription, new Date().toString(), false);
+            }).not.toThrow(Error);
+        }
+    });
+
+    test("Partition 3 Valid", () => {
+        for (let i = 0; i < 2; i++) {
+            let testDescription: string = "";
+            for (let j = 0; j < partition3[i]; j++) {
+                testDescription += "x";
+            }
+
+            expect(() => {
+                checkAddTaskBoundary("title", testDescription, new Date().toString(), false);
+            }).not.toThrow(Error);
+        }
+    });
+})
 
 function getRandomInt(min: number, max: number) {
     const minCeiled = Math.ceil(min);
