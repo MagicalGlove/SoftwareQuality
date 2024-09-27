20d5b03a12b390b22b524a8563e0244266e1b7ee
SMerved <90383268+SMerved@users.noreply.github.com>
2024-09-27 07:53:42 +0200

    Revert "."
    This reverts commit ac0cf2554a658f13b540d143bfec401cb04e1f2e.

--- ola3/Project/client/cypress.config.ts
+++ ola3/Project/client/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(_, __) {
+    setupNodeEvents(on, config) {
       // Implement node event listeners here
     },
     baseUrl: "http://localhost:3000",
